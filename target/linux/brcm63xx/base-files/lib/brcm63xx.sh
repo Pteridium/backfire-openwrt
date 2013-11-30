fe94d2562a096cd9fa9c0ae140cd2034b4ab9730
Álvaro Fernández Rojas <noltari@gmail.com>
2013-11-30 15:09:06 +0100

    [brcm63xx] Cleanup brcm63xx.sh.

--- target/linux/brcm63xx/base-files/lib/brcm63xx.sh
+++ target/linux/brcm63xx/base-files/lib/brcm63xx.sh
@@ -3,9 +3,6 @@
 board_name=""
 board_model=""
 status_led=""
-status_led2=""
-sys_mtd_part=""
-brcm63xx_has_reset_button=""
 ifname="eth0"
 
 brcm63xx_detect() {
@@ -22,7 +19,6 @@ brcm63xx_detect() {
 		board_model="Huawei HG520v"
 		;;
 	96368M-1541N)
-		brcm63xx_has_reset_button="true"
 		board_model="Comtrend VR-3025u"
 		status_led="VR-3025u:green:power"
 		;;
@@ -32,8 +28,8 @@ brcm63xx_detect() {
 	esac
 
 	[ -e "/tmp/sysinfo/" ] || mkdir -p "/tmp/sysinfo/"
-	echo "$board_name" > /tmp/sysinfo/board_name
-	echo "$board_model" > /tmp/sysinfo/model
+	[ -e "/tmp/sysinfo/board_name" ] || echo "$board_name" > /tmp/sysinfo/board_name
+	[ -e "/tmp/sysinfo/model" ] || echo "$board_model" > /tmp/sysinfo/model
 }
 
 brcm63xx_detect
