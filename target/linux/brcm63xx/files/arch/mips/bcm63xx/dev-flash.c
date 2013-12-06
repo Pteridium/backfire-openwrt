c0f059c495dcd46319ecbaaf92fb025776f1d333
Álvaro Fernández Rojas <noltari@gmail.com>
2013-12-06 18:30:53 +0100

    [brcm63xx] Switch to physmap.

--- target/linux/brcm63xx/files/arch/mips/bcm63xx/dev-flash.c
+++ target/linux/brcm63xx/files/arch/mips/bcm63xx/dev-flash.c
@@ -16,10 +16,27 @@
 #include <linux/mtd/partitions.h>
 #include <linux/mtd/physmap.h>
 
+#include <bcm63xx_board.h>
 #include <bcm63xx_cpu.h>
 #include <bcm63xx_dev_flash.h>
 #include <bcm63xx_regs.h>
 #include <bcm63xx_io.h>
+
+static struct mtd_partition mtd_partitions[] = {
+	{
+		.name		= "cfe",
+		.offset		= 0x0,
+		.size		= 0x40000,
+	}
+};
+
+static const char *bcm63xx_part_types[] = { "bcm63xxpart", NULL };
+
+static struct physmap_flash_data flash_data = {
+	.width			= 2,
+	.parts			= mtd_partitions,
+	.part_probe_types	= bcm63xx_part_types,
+};
 
 static struct resource mtd_resources[] = {
 	{
@@ -30,9 +47,12 @@ static struct resource mtd_resources[] = {
 };
 
 static struct platform_device mtd_dev = {
-	.name			= "bcm963xx-flash",
+	.name			= "physmap-flash",
 	.resource		= mtd_resources,
 	.num_resources		= ARRAY_SIZE(mtd_resources),
+	.dev			= {
+		.platform_data	= &flash_data,
+	},
 };
 
 static int __init bcm63xx_detect_flash_type(void)
--- target/linux/brcm63xx/files/drivers/mtd/bcm63xxpart.c
+++ target/linux/brcm63xx/files/drivers/mtd/bcm63xxpart.c
@@ -1,6 +1,10 @@
 /*
- * Copyright (C) 2006-2008  Florian Fainelli <florian@openwrt.org>
- * 			    Mike Albon <malbon@openwrt.org>
+ * BCM63XX CFE image tag parser
+ *
+ * Copyright © 2006-2008  Florian Fainelli <florian@openwrt.org>
+ *			  Mike Albon <malbon@openwrt.org>
+ * Copyright © 2009-2010  Daniel Dickinson <openwrt@cshore.neomailbox.net>
+ * Copyright © 2011 Jonas Gorski <jonas.gorski@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,52 +19,29 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
  */
 
 #include <linux/crc32.h>
-#include <linux/init.h>
+#include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/sizes.h>
-#include <linux/mtd/map.h>
-#include <linux/mtd/mtd.h>
-#include <linux/mtd/partitions.h>
 #include <linux/slab.h>
 #include <linux/vmalloc.h>
-#include <linux/platform_device.h>
-#include <linux/magic.h>
-#include <linux/jffs2.h>
-
-#include <asm/io.h>
+#include <linux/mtd/mtd.h>
+#include <linux/mtd/partitions.h>
 
 #include <asm/mach-bcm63xx/bcm63xx_nvram.h>
 #include <asm/mach-bcm63xx/bcm_tag.h>
 #include <asm/mach-bcm63xx/bcm63xx_board.h>
 
-#define BCM63XX_CFE_BLOCK_SIZE	SZ_64K		/* always at least 64KiB */
-
 #define BCM63XX_EXTENDED_SIZE	0xBFC00000	/* Extended flash address */
 
-#define BUSWIDTH 2                     /* Buswidth */
+#define BCM63XX_CFE_BLOCK_SIZE	SZ_64K		/* always at least 64KiB */
 
-#define PFX KBUILD_MODNAME ": "
-
-struct squashfs_super_block {
-	__le32 s_magic;
-	__le32 pad0[9];
-	__le64 bytes_used;
-};
-
-extern int parse_redboot_partitions(struct mtd_info *master, struct mtd_partition **pparts, unsigned long fis_origin);
-static struct mtd_partition *parsed_parts;
-
-static struct mtd_info *bcm963xx_mtd_info;
-
-static struct map_info bcm963xx_map = {
-       .name		= "bcm963xx",
-       .bankwidth	= BUSWIDTH,
-};
-
-static int parse_cfe_partitions( struct mtd_info *master, struct mtd_partition **pparts)
+static int bcm63xx_parse_cfe_partitions(struct mtd_info *master,
+					struct mtd_partition **pparts,
+					unsigned long origin)
 {
 	/* CFE, NVRAM and global Linux are always present */
 	int nrparts = 3, curpart = 0;
@@ -207,108 +188,28 @@ static int parse_cfe_partitions( struct mtd_info *master, struct mtd_partition *
 	return nrparts;
 };
 
-static int bcm963xx_probe(struct platform_device *pdev)
-{
-	int err = 0;
-	int parsed_nr_parts = 0;
-	char *part_type;
-	struct resource *r;
-
-	r = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	bcm963xx_map.phys = r->start;
-	bcm963xx_map.size = (r->end - r->start) + 1;
-	bcm963xx_map.virt = ioremap(r->start, r->end - r->start + 1);
-
-	if (!bcm963xx_map.virt) {
-		printk(KERN_ERR PFX "Failed to ioremap\n");
-		return -EIO;
-	}
-	printk(KERN_INFO PFX "0x%08lx at 0x%08x\n", bcm963xx_map.size, bcm963xx_map.phys);
-
-	simple_map_init(&bcm963xx_map);
-
-	bcm963xx_mtd_info = do_map_probe("cfi_probe", &bcm963xx_map);
-	if (!bcm963xx_mtd_info) {
-		printk(KERN_ERR PFX "Failed to probe using CFI\n");
-		err = -EIO;
-		goto err_probe;
-	}
-
-	bcm963xx_mtd_info->owner = THIS_MODULE;
-
-	/* This is mutually exclusive */
-	if (bcm63xx_is_cfe_present()) {
-		printk(KERN_INFO PFX "CFE bootloader detected\n");
-		if (parsed_nr_parts == 0) {
-			int ret = parse_cfe_partitions(bcm963xx_mtd_info, &parsed_parts);
-			if (ret > 0) {
-				part_type = "CFE";
-				parsed_nr_parts = ret;
-			}
-		}
-	} else {
-		printk(KERN_INFO PFX "assuming RedBoot bootloader\n");
-		if (bcm963xx_mtd_info->size > 0x00400000) {
-			printk(KERN_INFO PFX "Support for extended flash memory size : 0x%llx ; ONLY 64MBIT SUPPORT\n", bcm963xx_mtd_info->size);
-			bcm963xx_map.virt = (u32)(BCM63XX_EXTENDED_SIZE);
-		}
-
-#ifdef CONFIG_MTD_REDBOOT_PARTS
-		if (parsed_nr_parts == 0) {
-			int ret = parse_redboot_partitions(bcm963xx_mtd_info, &parsed_parts, 0);
-			if (ret > 0) {
-				part_type = "RedBoot";
-				parsed_nr_parts = ret;
-			}
-		}
-#endif
-	}
-
-	return add_mtd_partitions(bcm963xx_mtd_info, parsed_parts, parsed_nr_parts);
-
-err_probe:
-	iounmap(bcm963xx_map.virt);
-	return err;
-}
-
-static int bcm963xx_remove(struct platform_device *pdev)
-{
-	if (bcm963xx_mtd_info) {
-		del_mtd_partitions(bcm963xx_mtd_info);
-		map_destroy(bcm963xx_mtd_info);
-	}
-
-	if (bcm963xx_map.virt) {
-		iounmap(bcm963xx_map.virt);
-		bcm963xx_map.virt = 0;
-	}
-
-	return 0;
-}
-
-static struct platform_driver bcm63xx_mtd_dev = {
-	.probe	= bcm963xx_probe,
-	.remove = bcm963xx_remove,
-	.driver = {
-		.name	= "bcm963xx-flash",
-		.owner	= THIS_MODULE,
-	},
+static struct mtd_part_parser bcm63xx_cfe_parser = {
+	.owner = THIS_MODULE,
+	.parse_fn = bcm63xx_parse_cfe_partitions,
+	.name = "bcm63xxpart",
 };
 
-static int __init bcm963xx_mtd_init(void)
+static int __init bcm63xx_cfe_parser_init(void)
 {
-	return platform_driver_register(&bcm63xx_mtd_dev);
+	return register_mtd_parser(&bcm63xx_cfe_parser);
 }
 
-static void __exit bcm963xx_mtd_exit(void)
+static void __exit bcm63xx_cfe_parser_exit(void)
 {
-	platform_driver_unregister(&bcm63xx_mtd_dev);
+	deregister_mtd_parser(&bcm63xx_cfe_parser);
 }
 
-module_init(bcm963xx_mtd_init);
-module_exit(bcm963xx_mtd_exit);
+module_init(bcm63xx_cfe_parser_init);
+module_exit(bcm63xx_cfe_parser_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Broadcom BCM63xx MTD partition parser/mapping for CFE and RedBoot");
+MODULE_AUTHOR("Daniel Dickinson <openwrt@cshore.neomailbox.net>");
 MODULE_AUTHOR("Florian Fainelli <florian@openwrt.org>");
 MODULE_AUTHOR("Mike Albon <malbon@openwrt.org>");
+MODULE_AUTHOR("Jonas Gorski <jonas.gorski@gmail.com");
+MODULE_DESCRIPTION("MTD partitioning for BCM63XX CFE bootloaders");
